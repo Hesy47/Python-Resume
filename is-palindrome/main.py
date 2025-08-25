388c59f73c927fa95df0f7c689a0a886e84cf83a
Hesy47 <death19knight@gmail.com>
2025-08-25 09:39:12 +0330

    complete is_palindrome task

--- is-palindrome/main.py
+++ is-palindrome/main.py
@@ -1,2 +1,7 @@
 def is_palindrome(text: str) -> bool:
-    pass
+    palindrome_word="".join(word.lower() for word in text if word.isalnum())
+    return palindrome_word == palindrome_word[::-1]
+
+
+print(is_palindrome("hello"))
+print(is_palindrome("was it a car or a cat I saw?"))
