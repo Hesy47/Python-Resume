fe9177ab59e6fe6a2796835513bc8801148ff577
Hesy47 <death19knight@gmail.com>
2025-08-25 18:12:38 +0330

    top_two_frequent task done

---
+++ top-two-frequent/main.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+
+def top_two_frequent(nums: list[int]) -> list[int]:
+    counter = Counter(nums)
+
+    sorted_items = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+    print(sorted_items)
+
+    return [num for num, _ in sorted_items[:2]]
+
+
+print(top_two_frequent([1, 1, 2, 2, 3, 3, 3]))
+print(top_two_frequent([5, 5, 4, 4, 4, 3]))
+print(top_two_frequent([7, 7, 8, 8, 9]))
